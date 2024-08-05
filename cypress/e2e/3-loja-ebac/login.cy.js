713aefdb823f2c786a3da1fd03c8c3d238e6effa
Gabriel Thiego <gabrielthiego@gmail.com>
2024-08-05 20:52:16 -0300

    dados

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,7 +1,10 @@
 /// <referemne tuypes="cypress"/>
+
+const perfil = require('../../fixtures/perfil.json')
+
 describe('Funcionalidade: login', () =>{
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta/')
 
     });
 
@@ -14,7 +17,6 @@ describe('Funcionalidade: login', () =>{
         cy.get('#username').type('gabrielteste@teste.com.br')
         cy.get('#password').type('teste@123')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, gabrielteste (não é gabrielteste? Sair)')
 
     })
@@ -23,7 +25,6 @@ it('deve exibir uma mesngem de erro ao inserir ususario invalido', () => {
     cy.get('#username').type('gabrielinvalidoteste@teste.com.br')
     cy.get('#password').type('teste@123')
     cy.get('.woocommerce-form > .button').click()
-
     cy.get('.woocommerce-error > li').should('contain' , 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário')
     
 });
@@ -32,11 +33,22 @@ it('deve apaprecer mensagem de erro para senha invalida', () => {
     cy.get('#username').type('gabrielteste@teste.com.br')
     cy.get('#password').type('testeerro@123')
     cy.get('.woocommerce-form > .button').click()
-
     cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail gabrielteste@teste.com.br está incorreta. Perdeu a senha?')
     cy.get('.woocommerce-error > li').should('exist')
 });
+it('deve fazer login a parit da massa de dados criada', () => {
+    cy.get('#username').type(perfil.usuario)
+    cy.get('#password').type(perfil.senha)
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, gabrielteste (não é gabrielteste? Sair)')
 
-
-
+});
+it.only('deve fazer login a partir do fixture', () => {
+  cy.fixture ('perfil').then(dados =>{ 
+    cy.get('#username').type(dados.usuario )
+    cy.get('#password').type(dados.senha , {log:false})
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, gabrielteste (não é gabrielteste? Sair)')
+    })
+});
 })
